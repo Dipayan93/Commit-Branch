7065aa72363c359abb9d427317755c35f58adc38
Dipayan93 <dipayanchatterjee8@gmail.com>
2023-04-05 01:58:03 +0530

    visibility

--- dom.js
+++ dom.js
@@ -2,3 +2,6 @@ var items = document.getElementsByClassName('group-item1')
 items[0].style.fontWeight = 'Bold'
 var li = document.getElementsByTagName('li');
 li[4].style.backgroundColor = 'yellow'
+var qs = document.querySelectorAll('.list-group-item')
+qs[1].style.backgroundColor = 'green'
+qs[2].style.visibility = 'hidden' 
