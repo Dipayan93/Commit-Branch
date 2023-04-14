6bf9a5f9ef1cb0728a1c271e454e356c86bd5bf1
Dipayan93 <dipayanchatterjee8@gmail.com>
2023-04-14 09:05:38 +0530

    14thApril

--- dom.js
+++ dom.js
@@ -1,7 +1,12 @@
-var items = document.getElementsByClassName('group-item1')
+/*var items = document.getElementsByClassName('group-item1')
 items[0].style.fontWeight = 'Bold'
 var li = document.getElementsByTagName('li');
 li[4].style.backgroundColor = 'yellow'
 var qs = document.querySelectorAll('.list-group-item')
 qs[1].style.backgroundColor = 'green'
-qs[2].style.visibility = 'hidden' 
+qs[2].style.visibility = 'hidden' */
+var item = document.querySelector('#items')
+item.parentElement.style.backgroundColor = '#f4f4f4'
+item.lastElementChild.textContent = 'Hello 4'
+item.lastChild.style.backgroundColor = 'red'
+
