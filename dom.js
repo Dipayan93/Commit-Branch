9dcb797e7b037cade01e42d8810b5587b6b62109
Dipayan93 <dipayanchatterjee8@gmail.com>
2023-04-17 01:29:58 +0530

    Creating Nodes and Modifying Dom

--- dom.js
+++ dom.js
@@ -6,7 +6,19 @@ var qs = document.querySelectorAll('.list-group-item')
 qs[1].style.backgroundColor = 'green'
 qs[2].style.visibility = 'hidden' */
 var item = document.querySelector('#items')
-item.parentElement.style.backgroundColor = '#f4f4f4'
+item.parentElement.style.backgroundColor = "grey"
 item.lastElementChild.textContent = 'Hello 4'
-item.lastChild.style.backgroundColor = 'red'
-
+item.firstElementChild.style.backgroundColor = "yellow"
+console.log(item.lastChild)
+console.log(item.firstChild)
+item.nextElementSibling.style.color = "red";
+console.log(item.nextSibling)
+console.log(item.previousSibling)
+var it = document.querySelectorAll('#sib')
+it[2].previousElementSibling.textContent = 'New Item'
+var cEle = document.createElement('div')
+cEle.className = 'nClass'
+cEle.setAttribute('title', 'Hello')
+var nVar = document.createTextNode('World')
+cEle.appendChild(nVar)
+console.log(cEle)
