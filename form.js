76303a7c6a928dc2ef1fb0ce1ca19929d177ab6a
Dipayan93 <dipayanchatterjee8@gmail.com>
2023-05-05 08:40:15 +0530

    Form data local storage

--- form.js
+++ form.js
@@ -3,14 +3,31 @@ btn.addEventListener('click', (e) => {
     e.preventDefault();
     btn.style.background = 'red';
     document.querySelector('body').classList.add('bg-dark');
+
+    localStorage.setItem("Name",document.getElementById('name').value)
+    localStorage.setItem("Email",document.getElementById('email').value)
+    localStorage.setItem("Phone",document.getElementById('phone').value)
+    localStorage.setItem("Date",document.getElementById('date').value)
+    localStorage.setItem("Time",document.getElementById('time').value)
     });
 btn.addEventListener('mouseover', (e) => {
-//e.preventDefault();
-document.querySelector('#myform').style.background = '#ccc';
+e.preventDefault();
+document.querySelector('#myform').style.background = 'red';
+btn.style.background = 'red';
 document.querySelector('body').classList.add('bg-dark');
 });
 btn.addEventListener('mouseout', (e) => {
     //e.preventDefault();
-    document.querySelector('#myform').style.background = '#ccc';
+    document.querySelector('#myform').style.background = 'red';
+    btn.style.background = 'red';
     document.querySelector('body').classList.add('bg-white');
     });
+
+
+
+
+
+
+
+
+
